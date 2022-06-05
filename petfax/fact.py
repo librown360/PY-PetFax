8cd8d61dacfe220d47bbff400bf75b5f00297bce
Lisa Brown <lisaann493@verizon.net>
2022-06-05 19:31:12 -0400

    connected db

--- petfax/fact.py
+++ petfax/fact.py
@@ -1,14 +1,24 @@
 from flask import (Blueprint, render_template, request, redirect)
+from . import models
 
 bp = Blueprint('fact', __name__, url_prefix='/facts')
 
 @bp.route('/', methods=['GET','POST'])
 def index():
     if request.method == 'POST':
-        print(request.form)
-        return redirect('/facts')
-    return render_template('/facts/index.html')
+        submitter = request.form['submitter']
+        fact = request.form['fact']
 
-@bp.route('/new')
+        new_fact = models.Fact(submitter=submitter,fact=fact)
+        models.db.session.add(new_fact)
+        models.db.session.commit()
+
+        return redirect('/facts/')
+    
+    results = models.Fact.query.all()
+
+    return render_template('facts/index.html', facts=results)
+
+@bp.route('/new/')
 def new():
-    return render_template('/facts/new.html')
+    return render_template('facts/new.html')
